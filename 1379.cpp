66ea23a7acc26cf945565d7edf3dfcedf9628a51
Sabryna <sabryna.sousa1323@gmail.com>
2016-07-25 14:10:09 -0300

    Question 1379

---
+++ 1379.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+  
+  int a, b;
+
+  while ( cin >> a >> b && a != 0 && b != 0 ){
+    cout << (2 * a) - b << endl;
+  }  
+  return 0;
+}
