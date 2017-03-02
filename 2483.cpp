5ccaae241e8ac788729afe6f87df8f91aea869f4
Sabryna <sabryna.sousa1323@gmail.com>
2017-03-02 11:20:34 -0300

    Question 2483

---
+++ 2483.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+  int i;
+  cin >> i;
+  cout << "Feliz nat";
+  
+  for ( int j = 0; j < i; j++ ){
+    cout << "a";
+  }
+  cout << "l!" << endl;
+}
